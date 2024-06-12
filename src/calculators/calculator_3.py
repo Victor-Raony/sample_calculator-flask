b662885976b270eb366c35a489cb7810ce7a445c
Victor Raony <victor_raony12@hotmail.com>
2024-06-11 23:25:04 -0300

    Adding route Calculator3

---
+++ src/calculators/calculator_3.py
@@ -0,0 +1,48 @@
+from flask import request as FlaskRequest
+from typing import Dict, List
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+
+class Calculator3():
+  def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+    self.__driver_handler = driver_handler
+
+  def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
+    body = request.json
+    input_data = self.__validate_body(body)
+  
+    variance = self.__calculate_variance(input_data)
+    multiplication = self.__calculate_multiplication(input_data)
+    self.__verify_results(variance, multiplication)
+    
+    formated_response = self.__format_repsonse(variance)
+    return formated_response
+    
+  def __validate_body(self, body: Dict) -> List[float]:
+    if "numbers" not in body:
+      raise Exception("body mal formatado")
+    
+    input_data = body[ "numbers" ]
+    return input_data
+  
+  def __calculate_variance(self, numbers: List[float]) -> float:
+    variance = self.__driver_handler.variance(numbers)
+    return variance
+  
+  def __calculate_multiplication(self, numbers: List[float]) -> float:
+    multiplication = 1
+    for number in numbers: multiplication *= number
+    
+    return multiplication
+  
+  def __verify_results(self, variance: float, multiplication: float) -> None:
+    if variance < multiplication:
+      raise Exception("Falha no processo: Variancia menor que a multiplicação")
+    
+  def __format_repsonse(self, variance: float) -> Dict:
+    return{
+      "data": {
+        "Calculator" : 3,
+        "value": round(variance, 2),
+        "Sucess" : True
+      }
+    }
